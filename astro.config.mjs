cdffd9734d3de7e7c0cf68bf17cd25c8d1a578af
lvisfelip3 <luisfeipe.vera23@gmail.com>
2024-12-20 21:04:05 -0300

    Feature: switched webcoreui carousel to splidejs carousel and added tooltips from tippy js

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,18 +8,6 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    ssr: {
-        noExternal: ['webcoreui']
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                api: 'modern-compiler'
-            }
-        }
-    }
-  },
   integrations: [
     tailwind(), 
     react(), 
